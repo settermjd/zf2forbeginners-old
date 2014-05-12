e358d22e9be2d55e380d2333fa4d7779225b7986
Matthew Setter <matthew@maltblue.com>
2014-05-12 08:28:06 +0200

    Added the core ZF2 Skeleton app files

---
+++ config/autoload/global.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+return array(
+    'app' => array(
+        'name' => 'Baby Management Application',
+        'webmaster' => array(
+            'name' => 'Matthew Setter',
+            'email' => 'matthew@maltblue.com'
+        )
+    )
+);
