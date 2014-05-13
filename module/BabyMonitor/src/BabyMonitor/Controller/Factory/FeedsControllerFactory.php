85540fe7acc8917d375599c30515770626783246
Matthew Setter <matthew@maltblue.com>
2014-05-13 11:30:44 +0200

    Refactored the FeedsController to use constructor injection
    This way it's simpler and clearer to add dependencies.

---
+++ module/BabyMonitor/src/BabyMonitor/Controller/Factory/FeedsControllerFactory.php
@@ -0,0 +1,37 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * PHP version 5.4
+ *
+ * @category   CategoryName
+ * @package    PackageName
+ * @author     Matthew Setter <matthew@maltblue.com>
+ * @copyright  2014 Client/Author
+ * @see        Enter if required
+ * @since      File available since Release/Tag:
+ */
+
+namespace BabyMonitor\Controller\Factory;
+
+use Zend\ServiceManager\FactoryInterface,
+    Zend\ServiceManager\ServiceLocatorInterface,
+    Zend\Cache\Exception\ExtensionNotLoadedException,
+    Zend\ServiceManager\Exception\ServiceNotCreatedException,
+    BabyMonitor\Controller\FeedsController;
+
+class FeedsControllerFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $sm   = $serviceLocator->getServiceLocator();
+        $userTable = $sm->get('BabyMonitor\Tables\UserTable');
+        $controller = new FeedsController($userTable);
+
+        return $controller;
+    }
+}
