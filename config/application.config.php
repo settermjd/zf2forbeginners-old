be6f4ce0744970c9e35f2fe16af3bb8c4b353fdc
Matthew Setter <matthew@maltblue.com>
2014-05-15 11:43:29 +0200

    Added in logging support via monolog

--- config/application.config.php
+++ config/application.config.php
@@ -9,7 +9,8 @@ return array(
     'modules' => array(
         'Application',
         'BabyMonitor',
-        'SlmMail'
+        'SlmMail',
+        'EnliteMonolog'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
