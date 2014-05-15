bc92f9d019d9d0d75a1f379b187bbf1636c2c1d6
Matthew Setter <matthew@maltblue.com>
2014-05-15 11:49:04 +0200

    Added support for an application cache
    This is centred around using Redis as the backend cache, but provides a
    rather flexible approach. The only thing which is specific is using
    EnliteMonologService. Something doesn't seem quite right here. But for
    the time being, I'm leaving it in.

---
+++ config/autoload/babymonitor.global.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+  'cache' => array(
+      'adapter' => array(
+          'name' => 'redis',
+          'options' => array(
+              'server' => array(
+                  'host' => 'localhost',
+                  'port' => '6379',
+              )
+          )
+      ),
+      'plugins' => array(
+          // Don't throw exceptions on cache errors
+          'exception_handler' => array(
+              'throw_exceptions' => false
+          ),
+      )
+  )
+);
